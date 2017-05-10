82fcb1e3c4fb71d5322bfc8ec77a053eb7c57256
Rwing <rwing@rwing.cn>
2017-05-10 19:04:10 +0800

    add multi data source support

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 'use strict';
 
-const typeorm = require('./lib/typeorm');
 require("reflect-metadata");
+const assert = require('assert');
+const typeorm = require('typeorm');
 
 module.exports = app => {
-  if (app.config.typeorm.app) typeorm(app);
+  if (app.config.typeorm.app) {
+    app.beforeStart(async function () {
+      if (app.config.typeorm.clients) {
+        app.typeorm = {};
+        for (const id in typeorm.clients) {
+          app.typeorm[id] = await createOneClient(app.config.typeorm.clients[id], app);
+        }
+      } else {
+        app.typeorm = await createOneClient(app.config.typeorm.client, app);
+      }
+    });
+  }
 };
+
+async function createOneClient(config, app) {
+  assert(config.type && config.host && config.port && config.username && config.database,
+    `[egg-typeorm] 'type: ${config.type}', 'host: ${config.host}', 'port: ${config.port}', 'user: ${config.username}', 'database: ${config.database}' are required on config`);
+
+  app.coreLogger.info('[egg-typeorm] connecting %s@%s:%s/%s',
+    config.username, config.host, config.port, config.database);
+
+  const connection = await typeorm.createConnection({
+    driver: {
+      type: config.type,
+      host: config.host,
+      port: config.port,
+      username: config.username,
+      password: config.password,
+      database: config.database
+    },
+    entities: [
+      config.entityPath
+    ],
+    autoSchemaSync: config.autoSchemaSync
+  });
+
+  app.beforeStart(function* () {
+    const rows = yield connection.entityManager.query('select 1 as column1;');
+    const index = count++;
+    app.coreLogger.info(`[egg-typeorm] instance[${index}] status OK, typeorm result: ${rows[0].column1}`);
+  });
+  return await connection;
+}
